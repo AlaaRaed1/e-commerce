3ada0cfad4339139debcdf2d5dd3cfdfc2a1ed9f
AlaaRaed1 <alaaraed455@gmail.com>
2023-03-07 15:49:24 +0300

    Update Reducer.js

--- src/redux/Reducer.js
+++ src/redux/Reducer.js
@@ -1,6 +1,6 @@
 function getTotal() {
   const cart = JSON.parse(localStorage.getItem("cart")) === undefined ? [] :  JSON.parse(localStorage.getItem("cart"));
-  const total =  === undefined ? 0 : cart.map((item) => item.quantity).reduce((a, b) => a + b, 0);
+  const total =  cart.map((item) => item.quantity).reduce((a, b) => a + b, 0);
   return total;
 }
 
